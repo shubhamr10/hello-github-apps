f948f9b901bb77c53eaef7487906585a2c740306
Shubham Rauniyar <161189306+shubhamArmur@users.noreply.github.com>
2024-03-05 19:10:11 +0530

    Update WETH.sol

--- WETH.sol
+++ WETH.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 // updating this file and generating report
+// this is WETH contract file, use carefully
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WETH is ERC20 {
